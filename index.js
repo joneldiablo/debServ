e4dfd9c13f2a541e6a001eeac77ea2a684d6ee5b
Jonathan Diego Rodríguez Rodríguez <joneldiablo@users.noreply.github.com>
2017-05-19 17:59:22 -0500

    usando express mejor

--- index.js
+++ index.js
@@ -9,3 +9,32 @@
     console.log( "Server running at http://127.0.0.1:1337/" );
     terminal( app );
     console.log( "Web-terminal accessible at http://127.0.0.1:1337/terminal" );
+/*
+var express = require('express');
+var fs = require('fs');
+var scriptContent;
+fs.readFile('./script.js', 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    scriptContent = data;
+});
+var app = express();
+
+app.listen(1337, function() {
+    console.log('Example app listening on port 1337!')
+})
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function(req, res, next) {
+    res.send(scriptContent);
+});
+
+app.post('/', function(req, res, next) {
+    // Handle the post for this route
+});
+*/
